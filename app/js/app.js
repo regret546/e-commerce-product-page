965b0704fb81895c09668638e28a1d4fa132a888
regret546 <iregret546@gmail.com>
2024-05-21 19:13:09 +0800

    add background grey when hamburger navbar is roggle

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,24 @@
 const navBar = document.querySelector("#mainNav");
 const hamburgerMenu = document.querySelector("#ham-menu");
+const backgroundWrapper = document.querySelector("#bgWrapper");
 const buttons = document.querySelectorAll("[data-carousel-button]");
+let navbarIsActive = false;
 let currentIndex = 0;
 
 hamburgerMenu.addEventListener("click", function () {
+  if (!navbarIsActive) {
+    navbarIsActive = true;
+  } else {
+    navbarIsActive = false;
+  }
   hamburgerMenu.classList.toggle("active");
   navBar.classList.toggle("active");
+
+  if (!navbarIsActive) {
+    backgroundWrapper.classList.remove("background-wrapper");
+  } else {
+    backgroundWrapper.classList.add("background-wrapper");
+  }
 });
 
 buttons.forEach((button) => {
